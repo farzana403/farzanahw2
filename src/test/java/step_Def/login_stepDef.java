1d11563c7dc2e9546b9f8578e2419d0a90f1d0ea
lamia labib <farzanayasmin403@gmail.com>
2022-10-07 09:35:58 -0700

    feature file and testrunner and pom.xml

---
+++ src/test/java/step_Def/login_stepDef.java
@@ -0,0 +1,26 @@
+package step_Def;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+public class login_stepDef {
+    @Given("i am at TalentTEK Homepage")
+    public void iAmAtTalentTEKHomepage() {
+        
+    }
+
+    @And("i enter my invalid email address")
+    public void iEnterMyInvalidEmailAddress() {
+        
+    }
+
+    @And("i enter my valid password")
+    public void iEnterMyValidPassword() {
+        
+    }
+
+    @When("i click on Login button")
+    public void iClickOnLoginButton() {
+    }
+}
---
+++ src/test/java/step_Def/profile_stepDef.java
@@ -0,0 +1,9 @@
+package step_Def;
+
+import io.cucumber.java.en.Then;
+
+public class profile_stepDef {
+    @Then("i should not be able to successfully login")
+    public void iShouldNotBeAbleToSuccessfullyLogin() {
+    }
+}
